0c5df7b0aecdf13d5f3e84cedc339fe412a1d230
Kseniaveh <31632695+Kseniaveh@users.noreply.github.com>
2018-12-17 22:59:47 +0300

    Update SM_rabota.py

--- SM_rabota.py
+++ SM_rabota.py
@@ -1,8 +1,20 @@
+#Инвариантная СР
+#2.1 Разработать прототип программы «Калькулятор», позволяющую выполнять базовые арифметические действия и функцию обертку, 
+#сохраняющую название выполняемой операции, аргументы и результат в файл
+#2.2 Дополнение программы «Калькулятор» декоратором, сохраняющий действия, которые выполняются в файл-журнал.
+#2.3 Рефакторинг (модификация) программы с декоратором модулем functools
+#2.4 Формирование отчета по практическому заданию и публикация его в портфолио.
+
+#Вариативная СР
+#2.3 Разработка функции-декоратора, вычисляющей время выполнения декорируемой функции.
+
 def get_valutes():
-    import urllib.request
-    from xml.etree import ElementTree #для работы с XML файлами
+    import urllib.request #определяет функции и классы , которые помогают в открытии URL. Откройте URL-адрес, 
+    #который может быть либо строкой, либо Request объектом.
+    from xml.etree import ElementTree as ET #для работы с XML файлами
     
-    resp = ElementTree.parse(urllib.request.urlopen("http://www.cbr.ru/scripts/XML_daily.asp")) #распарсиваем данные с сайта 
+    resp = ET.parse(urllib.request.urlopen("http://www.cbr.ru/scripts/XML_daily.asp"))
+    #распарсиваем данные с сайта 
     valutes = {}
     #findall - находит только элементы с тегом, которые являются прямыми потомками текущего элемента
     for row in resp.findall('Valute'):
@@ -10,37 +22,47 @@ def get_valutes():
     valutes.update({'RUB': 1})
     return valutes
 
+
 """Функций для постановки запятой в нужном месте"""
 def print_valutes(valutes):
     result = ''
     i = 0
     for valute in valutes:
-        if (i > 0):
-            result += ',' + valute
+        if (i>0):
+            result+= ','+valute
         else:
-            result += valute        
-        i += 1
+            result+= valute        
+        i+= 1
     print (result)
 
 
-def write_log(func):
+def write_log(func):# в качестве аргумента передается функция calculate_valute
     import functools
+    import datetime
+    import time    
     #функция обертка
     @functools.wraps(func)
-    def wrapper(*args):
+    def wrapper(*args): #  В *args сложены все аргументы функции calculate_valute
+        timeStart = datetime.datetime.now()
+        #time.sleep(2)
         result = func(*args)
-        with open("logger.txt", "a") as f:
+        timeEnd = datetime.datetime.now()
+        with open("log.txt", "a") as f:
             f.write(("*" * 30) + "\n")
+            f.write("Начало: " + str(timeStart) + "\n")
             f.write(args[3]+": " + str(args[2]) + "\n")
             f.write(args[4]+": " + str(result) + "\n")
+            f.write("Конец: " + str(timeEnd) + "\n")
+            f.write("Время выполнения: " + str(timeEnd - timeStart) + "\n")
             f.write(("*" * 30) + "\n")            
         return result
     return wrapper
-    
+
+"""Функция конвертирующая валюту, которая декорируется логгером """    
 @write_log
-def calculate_valute(fromValuteValue,toValuteValue,unit,fromValute,toValute):
+def calculate_valute(fromValuteValue,toValuteValue,edinic,fromValute,toValute):
     koff = fromValuteValue / toValuteValue
-    result = unit*koff
+    result = edinic * koff
     return result
 
 if __name__ == "__main__":
@@ -50,25 +72,26 @@ if __name__ == "__main__":
         
     while True:
         try:
-            fromValute = input("Выберите конвертируемую валюту: ")  
+            fromValute = input("Выберите ковертируемую валюту: ")  
             toValute = input("Выберите конечную валюту: ") 
             
             fromValuteValue = valutes[fromValute]
             toValuteValue = valutes[toValute]
         except (KeyError):
-            print("Таких валют не существует, убедитесь что вы правильно ввели")
+            print("Таких валют не существует, убедитесь что вы правильно ввели.")
             #созданы с целью того, чтобы программа не зацикливалась
             continue
         break
     
     while True:
         try:
-            money = float(input("Введите количество единиц валюты: "))
+            Money = float(input("введите количество едениц валюты: "))
         except (TypeError, ValueError):
-            print("Вы ввели неправильное значение, нужно вводить число")
+            print("Вы ввели неправильное значение, нужно вводить число.")
             continue
         break
     
-    resultMoney = calculate_valute(fromValuteValue,toValuteValue, money,fromValute,toValute)
+    resultMoney = calculate_valute(fromValuteValue,toValuteValue,Money,fromValute,toValute)
     
-    print (fromValute+": "+str(money)+" = "+toValute+": "+str(resultMoney))
+    print (fromValute+": "+str(Money)+" = "+toValute+": "+str(resultMoney))
+    
